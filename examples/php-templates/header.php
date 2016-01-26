8b46d441ccdaedda5b5c000195e2ea8de81ce953
john <07jfxiao@gmail.com>
2016-01-26 16:57:32 +0800

    Remove redundancy in example

--- examples/php-templates/header.php
+++ examples/php-templates/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <title>test</title>
-  <link rel="stylesheet" href="static/dist/app.fb3178f4fc6a1781edaf5b2b21307471.css" />
+  <link rel="stylesheet" href="static/dist/app.1c451d2dd547f4e0dcb2b83f4ec658b9.css" />
   <link rel="stylesheet" href="static/dist/desktop.67c21c3cf976afbdec1b7a2bad65e7a0.css" />
 
 </head>
